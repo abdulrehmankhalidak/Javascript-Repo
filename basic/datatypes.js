74cffb62b5a99197d0a5ec7d9d24ad2f541538a2
Abdulrehman khalid <154405091+abdulrehmankhalidak@users.noreply.github.com>
2024-03-21 11:32:09 +0000

    operations in js

--- basic/datatypes.js
+++ basic/datatypes.js
@@ -12,15 +12,18 @@
 // console.log(typeof null);
 
 // conversion operation in javascript "strict"
-let score= null
-console.log(typeof score)
+// let score= null
+// console.log(typeof score)
 
-let valueNumber = Number(score)
-console.log( typeof valueNumber)
-console.log(valueNumber)
+// let valueNumber = Number(score)
+// console.log( typeof valueNumber)
+// console.log(valueNumber)
 
 
 // "33" => 33
 // 3abc => NaN
 // true => 1 and false=>0
+
+///////////////////////////operation in javascript////////////////////
+
 

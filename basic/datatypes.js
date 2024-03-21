6e06d1f4dcf6959578a436e0f60a8bcc86929f78
Abdulrehman khalid <154405091+abdulrehmankhalidak@users.noreply.github.com>
2024-03-21 10:13:10 +0000

    Conversion in number and string in jss

--- basic/datatypes.js
+++ basic/datatypes.js
@@ -9,4 +9,18 @@
 
 //object
 
-console.log(typeof null);
+// console.log(typeof null);
+
+// conversion operation in javascript "strict"
+let score= null
+console.log(typeof score)
+
+let valueNumber = Number(score)
+console.log( typeof valueNumber)
+console.log(valueNumber)
+
+
+// "33" => 33
+// 3abc => NaN
+// true => 1 and false=>0
+

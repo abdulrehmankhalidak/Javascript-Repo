79f303180999c5e78e3a89e75cfe9154334acca1
Abdulrehman khalid <154405091+abdulrehmankhalidak@users.noreply.github.com>
2024-03-21 07:55:25 +0000

    working with variables let and const

--- basic/var.js
+++ basic/var.js
@@ -1,1 +1,17 @@
-console.log("haris")
+const accountid = 1456
+let  accountEmail = "haris@gmail.com"
+var accountPassword = "123456"
+accountCity =  "FSD"
+
+
+// accountid = 2  not allowed 
+
+ accountEmail = "ha@gmail.com"
+accountPassword = "123456789"
+accountCity =  "lhr"
+
+/*
+prefer not to use var cos of block scope issues
+*/
+
+ console.table([accountEmail,accountid,accountPassword,accountCity])
